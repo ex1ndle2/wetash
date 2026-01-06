107144024efe3cf5563a2d1822d1537987e5d59a
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-06 18:50:04 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-LOGIN_REDIRECT_URL = '/'  # После входа перенаправлять на главную
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'login' 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
