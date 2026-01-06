5588b75454094b8f2dd91452619ee9b5707021b7
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-06 18:44:10 +0500

    .;

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import dj_database_url
-import os
+
 
 # Заменяет ваш блок DATABASES
 DATABASES = {
