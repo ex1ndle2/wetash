62015c919687c85b6222b94b8a47111a7d5ac121
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-06 23:01:03 +0500

    .

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+import core.models as core_models
 # Register your models here.
+admin.site.register(core_models.User)
+admin.site.register(core_models.Family)
+admin.site.register(core_models.AirQuality)     
+admin.site.register(core_models.Quest)  
+admin.site.register(core_models.QuestCompletion)  
+admin.site.register(core_models.Reward) 
+admin.site.register(core_models.RewardRedemption)
